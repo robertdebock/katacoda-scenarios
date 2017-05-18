6522988e5102ca416e85f950f25f76f67d1668f3
Robert de Bock <robert@meinit.nl>
2017-05-18 16:59:33 +0200

    Adding better test.

--- fun-with-docker/step1-verify.sh
+++ fun-with-docker/step1-verify.sh
@@ -1,1 +1,1 @@
-if [ $(docker ps -a --filter ancestor=hello-world | wc -l) = 2 ] ; then echo "done" ; fi
+if [ $(docker ps -a --filter ancestor=hello-world | wc -l) = 2 ] ; then echo "done" ; else exit 1; fi
