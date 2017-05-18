bfe6b331acc0dffbee34ac6a288eba9adcd7b118
Robert de Bock <robert@meinit.nl>
2017-05-18 17:39:51 +0200

    Debug.

--- fun-with-docker/step1-verify.sh
+++ fun-with-docker/step1-verify.sh
@@ -1,2 +1,6 @@
-[ -d /home/scrapbook/tutorial/test ] && echo "done"
-#docker ps -a | grep hello-world > /dev/null && echo "done"
+#!/bin/sh
+
+echo "1" > /tmp/test
+#[ -d /home/scrapbook/tutorial/test ] && echo "done"
+docker ps -a | grep hello-world > /dev/null && echo "done"
+echo "2" >> /tmp/test
