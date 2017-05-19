15cdf17bf1f17efb17ff2b4e8cd272708ac7a731
Robert de Bock <robert@meinit.nl>
2017-05-19 15:14:57 +0200

    Different verify.

--- fun-with-docker/step5-verify.sh
+++ fun-with-docker/step5-verify.sh
@@ -1,4 +1,4 @@
 cmd=$(docker inspect  -f "{{.Config.Cmd}}" $(docker ps -ql))
 image=$(docker inspect  -f "{{.Config.Image}}" $(docker ps -ql))
 
-[ "${cmd}" = '[/bin/sh]' -a "${image}" = 'myimage' ] && echo -n "done"
+[ "${cmd}" = '[/bin/sh]' -a "${image}" = 'myimage' ] && echo "done"
