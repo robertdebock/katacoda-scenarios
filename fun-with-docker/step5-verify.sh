21c5ec2dde3495dac312b4cf245cb595a8554884
Robert de Bock <robert@meinit.nl>
2017-05-19 10:07:53 +0200

    Changing test.

--- fun-with-docker/step5-verify.sh
+++ fun-with-docker/step5-verify.sh
@@ -1,5 +1,5 @@
-cmd=$(docker inspect  -f "{{.Config.Cmd}}" $(docker ps -aql))
-image=$(docker inspect  -f "{{.Config.Image}}" $(docker ps -aql))
+cmd=$(docker inspect  -f "{{.Config.Cmd}}" $(docker ps -ql))
+image=$(docker inspect  -f "{{.Config.Image}}" $(docker ps -ql))
 
 if [ "${cmd}" = '[/bin/sh]' -a "${image}" = 'myimage' ] ; then
   echo "done"
