0b37568b9cd6bd19fda8419f21bcbd886ca90416
Robert de Bock <robert@meinit.nl>
2017-05-21 07:45:46 +0200

    Simpler check to verify.

--- fun-with-docker/step5-verify.sh
+++ fun-with-docker/step5-verify.sh
@@ -1,9 +1,1 @@
-file=step5-debug.txt
-
-cmd=$(docker inspect  -f "{{.Config.Cmd}}" $(docker ps -ql))
-echo "cmd = ${cmd}" >> "${file}"
-
-image=$(docker inspect  -f "{{.Config.Image}}" $(docker ps -ql))
-echo "image = ${image}" >> "${file}"
-
-[ "${cmd}" = '[/bin/sh]' -a "${image}" = 'myimage' ] && echo "done" | tee -a "${file}"
+docker ps -al | grep myimage | grep '/bin/sh' > /dev/null 2>&1 && echo "done"
