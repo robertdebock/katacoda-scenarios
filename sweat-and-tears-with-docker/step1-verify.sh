616c4fbb523abf6f43ed550dc3679aa64eb49155
Robert de Bock <robert@meinit.nl>
2017-05-22 20:06:38 +0200

    Debug.

--- sweat-and-tears-with-docker/step1-verify.sh
+++ sweat-and-tears-with-docker/step1-verify.sh
@@ -3,6 +3,9 @@ docker inspect $(docker ps -alq) > output
 cmd=$(docker run -v $(pwd):/data realguess/jq jq -r '.[0].Config.Cmd[0]' /data/output)
 env=$(docker run -v $(pwd):/data realguess/jq jq -r '.[0].Config.Env[0]' /data/output)
 
+echo "${cmd}" > "${cmd}"
+echo "${env}" > "${env}"
+
 if [ "${cmd}" = "printenv" -a "${env}" = "variable=value" ] ; then
   echo "done"
   rm output
