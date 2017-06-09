835b35d9aebd637f341bb0bc857b7543d331415d
Robert de Bock <robert@meinit.nl>
2017-06-09 20:02:21 +0200

    Fixing verify

--- sweat-and-tears-with-docker/variables-verify.sh
+++ sweat-and-tears-with-docker/variables-verify.sh
@@ -1,2 +1,1 @@
-#docker inspect $(docker ps -alq) | grep 'variable=value' > /dev/null 2>&1 && echo "done"
-echo "done"
+docker inspect $(docker ps -alq) | grep 'variable=value' > /dev/null 2>&1 && echo "done"
