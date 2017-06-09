0fe729151e9444d5db88661e1b0374faeb119e38
Robert de Bock <robert@meinit.nl>
2017-06-09 16:33:04 +0200

    Create variables-verify.sh

--- sweat-and-tears-with-docker/variables-verify.sh
+++ sweat-and-tears-with-docker/variables-verify.sh
@@ -1,12 +1,1 @@
-docker inspect $(docker ps -alq) > output
-
-cmd=$(docker run -v $(pwd):/data realguess/jq jq -r '.[0].Config.Cmd[0]' /data/output)
-env=$(docker run -v $(pwd):/data realguess/jq jq -r '.[0].Config.Env[0]' /data/output)
-
-echo "${cmd}" > "${cmd}"
-echo "${env}" > "${env}"
-
-if [ "${cmd}" = "printenv" -a "${env}" = "variable=value" ] ; then
-  echo "done"
-  rm output
-fi
+docker inspect $(docker ps -alq) | grep 'variable=value' > /dev/null 2>&1 && echo "done"
