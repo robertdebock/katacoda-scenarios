7c4fca4b4dd39fa1eb498a5c667a1510ce17dda1
Robert de Bock <robert@meinit.nl>
2017-06-10 06:53:40 +0200

    Fixing 2 verify scripts.

--- sweat-and-tears-with-docker/compose-verify.sh
+++ sweat-and-tears-with-docker/compose-verify.sh
@@ -1,1 +1,1 @@
-echo "done"
+docker ps --filter "name=tutorial_wordpress_1" > /dev/null 2>&1 && docker ps --filter "name=tutorial_db_1" > /dev/null 2>&1 && echo "done"
--- sweat-and-tears-with-docker/volumes-verify.sh
+++ sweat-and-tears-with-docker/volumes-verify.sh
@@ -1,1 +1,1 @@
-docker exec $(docker ps -ql) ls -l /usr/local/apache2/htdocs/index.html > /dev/null 2>&1 && echo "done"
+docker exec $(docker ps -ql) cat /usr/local/apache2/htdocs/index.html | grep 'I did it' > /dev/null 2>&1 && echo "done"
