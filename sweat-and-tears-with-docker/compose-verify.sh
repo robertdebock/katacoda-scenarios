947fdc3d1efe6071a4fe56ff0e1e1e2a5c3e048e
Robert de Bock <robert@meinit.nl>
2017-06-10 06:59:24 +0200

    Fixing compose-verify and deamonizing httpd.

--- sweat-and-tears-with-docker/compose-verify.sh
+++ sweat-and-tears-with-docker/compose-verify.sh
@@ -1,1 +1,1 @@
-docker ps --filter "name=tutorial_wordpress_1" > /dev/null 2>&1 && docker ps --filter "name=tutorial_db_1" > /dev/null 2>&1 && echo "done"
+docker ps | grep "name=tutorial_wordpress_1" > /dev/null 2>&1 && docker ps | grep "name=tutorial_db_1" > /dev/null 2>&1 && echo "done"
