a113ad2623149a28e5d2a1eb039163c2f87ff9f5
Robert de Bock <robert@meinit.nl>
2017-06-10 07:05:30 +0200

    Fixing verify

--- sweat-and-tears-with-docker/compose-verify.sh
+++ sweat-and-tears-with-docker/compose-verify.sh
@@ -1,1 +1,1 @@
-docker ps | grep "name=tutorial_wordpress_1" > /dev/null 2>&1 && docker ps | grep "name=tutorial_db_1" > /dev/null 2>&1 && echo "done"
+docker ps | grep "tutorial_wordpress_1" > /dev/null 2>&1 && docker ps | grep "tutorial_db_1" > /dev/null 2>&1 && echo "done"
