494122775b271a9c01fd1f71c9fca718cec1f799
Robert de Bock <robert@meinit.nl>
2019-01-16 18:59:30 +0000

    Simpler.

--- developing-ansible-roles/step2-verify.sh
+++ developing-ansible-roles/step2-verify.sh
@@ -1,10 +1,8 @@
-cat << EOF >> /tmp/reference.yml
----
+echo "---
 # tasks file for timesync
 - name: install chrony
   package:
     name: chrony
-    state: present
-EOF
+    state: present" > /tmp/reference.yml
 
 diff /tmp/reference.yml /home/scrapbook/tutorial/timesync/tasks/main.yml > /dev/null && echo "done"
