65e83e3314a42a755a086495b61ef26e5a7fba6e
Robert de Bock <robert@meinit.nl>
2019-01-16 18:49:21 +0000

    Simpler test.

--- developing-ansible-roles/step1-verify.sh
+++ developing-ansible-roles/step1-verify.sh
@@ -1,10 +1,1 @@
-result="okay"
-
-test -d /home/scrapbook/tutorial/timesync || result="failed"
-test -d /home/scrapbook/tutorial/timesync/defaults || result="failed"
-test -d /home/scrapbook/tutorial/timesync/meta || result="failed"
-test -d /home/scrapbook/tutorial/timesync/tasks || result="failed"
-test -d /home/scrapbook/tutorial/timesync/templates || result="failed"
-test -d /home/scrapbook/tutorial/timesync/vars || result="failed"
-
-echo "${result}"
+[ -d /home/scrapbook/tutorial/timesync ] && echo "okay"
