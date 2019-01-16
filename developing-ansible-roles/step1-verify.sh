3dc30e63b0d1c0308f838c564340435c1bd5eb21
Robert de Bock <robert@meinit.nl>
2019-01-16 11:03:09 +0000

    Test for -directories-.

--- developing-ansible-roles/step1-verify.sh
+++ developing-ansible-roles/step1-verify.sh
@@ -1,1 +1,1 @@
-(test -d timesync/ ; test -d timesync/defaults/ ; test timesync/meta ; test timesync/tasks ; test timesync/templates ; test timesync/vars ) && echo "done"
+(test -d timesync/ ; test -d timesync/defaults/ ; test -d timesync/meta ; test -d timesync/tasks ; test -d timesync/templates ; test -d timesync/vars) && echo "done"
