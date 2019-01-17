dc443eb672d7dfdc90d6dddfc12d119aa757acba
Robert de Bock <robert@meinit.nl>
2019-01-17 20:04:53 +0000

    Update setuptools.

--- developing-ansible-roles/intro-code.sh
+++ developing-ansible-roles/intro-code.sh
@@ -1,1 +1,5 @@
-sudo apt-get update && sudo apt-get -y upgrade &&  sudo apt-get -y install python-pip python-dev libssl-dev && pip install ansible
+sudo apt-get update && \
+sudo apt-get -y upgrade && \
+sudo apt-get -y install python-pip python-dev libssl-dev && \
+pip install --ignore-installed setuptools -U && \
+pip install ansible
