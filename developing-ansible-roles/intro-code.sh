509d36d1aac243d58577ef2dab7e3e65f92f5f85
Robert de Bock <robert@meinit.nl>
2019-06-21 09:06:36 +0200

    Image ansible should not need preparation.

--- developing-ansible-roles/intro-code.sh
+++ developing-ansible-roles/intro-code.sh
@@ -1,5 +1,1 @@
-sudo apt-get update && \
-sudo apt-get -y upgrade && \
-sudo apt-get -y install python-pip python-dev libssl-dev && \
-pip install --ignore-installed setuptools -U && \
-pip install ansible
+echo "Ready"
